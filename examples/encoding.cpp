2f3b3902cbbf6baaebc96fbb619672dc19b2833c
Cos8o <help.ps.cos8o@gmail.com>
2018-08-29 22:26:33 +0200

    Update encoding.cpp

--- examples/encoding.cpp
+++ examples/encoding.cpp
@@ -4,13 +4,10 @@ using namespace std;
 
 int main()
 {
-	
+
 	vector<unsigned char> Password = { 'H', 'e', 'l', 'l', 'o' };
 
-	cout 
-		<< "Encoded data: "
-		<< RobtopCipher_Encode(Password, GJP_KEY) 
-		<< endl;
+	cout << "Encoded data: " << RobtopCipher_Encode(Password, GJP_KEY) << endl;
 
 	cin.get();
 	return 0;
