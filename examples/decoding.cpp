a9da982336a73d39961c619a671e9b03686e1465
Cos8o <help.ps.cos8o@gmail.com>
2018-08-29 22:24:49 +0200

    Decoding example
    Decoding data using GDCrypto. This code uses the GJP key, which is used to encode passwords. More keys are available, for more info read the header file.

---
+++ examples/decoding.cpp
@@ -0,0 +1,16 @@
+#include "include/gdcrypto.hpp"
+
+using namespace std;
+
+int main()
+{
+	string encPwd("fk5lU0VAQFpAUgIFBg==");
+	vector<unsigned char> decPwd = RobtopCipher_Decode(encPwd, GJP_KEY);
+	string decPwdS(decPwd.begin(), decPwd.end());
+  
+	cout << "Encoded Password: " << encPwd << endl;
+	cout << "Decoded Password: " << decPwdS << endl;
+  
+  cin.get();
+  return 0;
+}
