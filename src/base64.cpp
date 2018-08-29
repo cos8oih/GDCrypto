5d137d3d2d2b8b9e1adbb44fefeba4ae1af805d0
Cos8o <help.ps.cos8o@gmail.com>
2018-08-29 23:32:12 +0200

    Update base64.cpp

--- src/base64.cpp
+++ src/base64.cpp
@@ -1,74 +1,7 @@
-/*
-* Base64 encoding (RFC1341)
-* Copyright (c) 2005-2011, Jouni Malinen <j@w1.fi>
-*
-* This software may be distributed under the terms of the BSD license.
-* See README for more details.
-*/
-
-// 2016-12-12 - Gaspard Petit : Slightly modified to return a std::string 
-// instead of a buffer allocated with malloc.
-
 #include "../include/base64.hpp"
 
 static const unsigned char base64_table[65] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
-
-/**
-* base64_encode - Base64 encode
-* @src: Data to be encoded
-* @len: Length of the data to be encoded
-* @out_len: Pointer to output length variable, or %NULL if not used
-* Returns: Allocated buffer of out_len bytes of encoded data,
-* or empty string on failure
-*/
-std::string base64_encode(const unsigned char *src, size_t len)
-{
-	unsigned char *out, *pos;
-	const unsigned char *end, *in;
-
-	size_t olen;
-
-	olen = 4 * ((len + 2) / 3);
-
-	if (olen < len)
-		return std::string();
-
-	std::string outStr;
-	outStr.resize(olen);
-	out = (unsigned char*)&outStr[0];
-
-	end = src + len;
-	in = src;
-	pos = out;
-	while (end - in >= 3) {
-		*pos++ = base64_table[in[0] >> 2];
-		*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
-		*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
-		*pos++ = base64_table[in[2] & 0x3f];
-		in += 3;
-	}
-
-	if (end - in) {
-		*pos++ = base64_table[in[0] >> 2];
-		if (end - in == 1) {
-			*pos++ = base64_table[(in[0] & 0x03) << 4];
-			*pos++ = '=';
-		}
-		else {
-			*pos++ = base64_table[((in[0] & 0x03) << 4) |
-				(in[1] >> 4)];
-			*pos++ = base64_table[(in[1] & 0x0f) << 2];
-		}
-		*pos++ = '=';
-	}
-
-	return outStr;
-}
-
-/*
-Decoding snippet by polfosol, slightly edited
-*/
 
 static const int B64index[256] = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -78,32 +11,63 @@ static const int B64index[256] = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
 0,  0,  0, 63,  0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
 
-std::string base64_decode(const void* data, const size_t len, size_t * lenOut)
+std::string Base64_Encode(const std::vector<unsigned char>& Src)
 {
-	unsigned char* p = (unsigned char*)data;
-	int pad = len > 0 && (len % 4 || p[len - 1] == '=');
-	const size_t L = ((len + 3) / 4 - pad) * 4;
-	std::string str(L / 4 * 3 + pad, '\0');
+	std::string Result;
+	auto In = Src.begin();
 
-	for (size_t i = 0, j = 0; i < L; i += 4)
+	while (Src.end() - In >= 3) 
 	{
-		int n = B64index[p[i]] << 18 | B64index[p[i + 1]] << 12 | B64index[p[i + 2]] << 6 | B64index[p[i + 3]];
-		str[j++] = n >> 16;
-		str[j++] = n >> 8 & 0xFF;
-		str[j++] = n & 0xFF;
+		Result += base64_table[In[0] >> 2];
+		Result += base64_table[((In[0] & 0x03) << 4) | (In[1] >> 4)];
+		Result += base64_table[((In[1] & 0x0f) << 2) | (In[2] >> 6)];
+		Result += base64_table[In[2] & 0x3f];
+		In += 3;
 	}
-	if (pad)
-	{
-		int n = B64index[p[L]] << 18 | B64index[p[L + 1]] << 12;
-		str[str.size() - 1] = n >> 16;
 
-		if (len > L + 2 && p[L + 2] != '=')
+	if (Src.end() - In) 
+	{
+		Result += base64_table[In[0] >> 2];
+		if (Src.end() - In == 1) {
+			Result += base64_table[(In[0] & 0x03) << 4];
+			Result += '=';
+		}
+		else 
 		{
-			n |= B64index[p[L + 2]] << 6;
-			str.push_back(n >> 8 & 0xFF);
+			Result += base64_table[((In[0] & 0x03) << 4) |
+				(In[1] >> 4)];
+			Result += base64_table[(In[1] & 0x0f) << 2];
+		}
+		Result += '=';
+	}
+
+	return Result;
+}
+
+std::vector<unsigned char> Base64_Decode(const std::string& Src)
+{
+	int Pad = Src.length() > 0 && (Src.length() % 4 || Src[Src.length() - 1] == '=');
+	const size_t L = ((Src.length() + 3) / 4 - Pad) * 4;
+	std::vector<unsigned char> Result;
+
+	for (size_t i = 0; i < L; i += 4)
+	{
+		int n = B64index[Src[i]] << 18 | B64index[Src[i + 1]] << 12 | B64index[Src[i + 2]] << 6 | B64index[Src[i + 3]];
+		Result.push_back(n >> 16);
+		Result.push_back(n >> 8 & 0xFF);
+		Result.push_back(n & 0xFF);
+	}
+	if (Pad)
+	{
+		int n = B64index[Src[L]] << 18 | B64index[Src[L + 1]] << 12;
+		Result.push_back(n >> 16);
+
+		if (Src.length() > L + 2 && Src[L + 2] != '=')
+		{
+			n |= B64index[Src[L + 2]] << 6;
+			Result.push_back(n >> 8 & 0xFF);
 		}
 	}
-	if (lenOut != nullptr)
-		*lenOut = L / 4 * 3 + pad;
-	return str;
+
+	return Result;
 }
