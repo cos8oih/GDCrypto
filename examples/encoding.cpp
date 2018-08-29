92462a4705b842d738b68615841dcb313655abf7
Cos8o <help.ps.cos8o@gmail.com>
2018-08-29 21:59:19 +0200

    Encoding example
    Encoding data using GDCrypto. This code uses the GJP key, which is used to encode passwords. More keys are available, for more info read the header file.

---
+++ examples/encoding.cpp
@@ -0,0 +1,17 @@
+#include "include/gdcrypto.hpp"
+
+using namespace std;
+
+int main()
+{
+	
+	vector<unsigned char> Password = { 'H', 'e', 'l', 'l', 'o' };
+
+	cout 
+		<< "Encoded data: "
+		<< RobtopCipher_Encode(Password, GJP_KEY) 
+		<< endl;
+
+	cin.get();
+	return 0;
+}
